ef981dd45a9ca25a933f7a1f570513c55a8da8ab
Eddo Dava <eddodava@gmail.com>
2024-09-13 08:48:51 +0700

    Pengantar bg3 : Praktikum 2

---
+++ Pengantar Mobile bagian 3/praktikum2.dart
@@ -0,0 +1,19 @@
+
+void main() {
+  //langkah 1
+  var halogens = {'fluorine', 'chlorine', 'bromine', 'iodine', 'astatine'};
+  print(halogens);
+  //langkah 2 : pada langkah 1 merupakan pembuatan variabel halogens dengan tipe data set yg memiliki 5 value
+  //langkah 3
+ /// The code snippet is demonstrating the creation and usage of sets and maps in Dart.
+  var names1 = <String>{};
+  Set<String> names2 = {}; // This works, too.
+  var names3 = {}; // Creates a map, not a set.
+  names1.add("Eddo Dava Alfarisi");
+  names2.addAll(["2241720232"]);
+  print(names1);
+  print(names2);
+  print(names3);
+
+  
+}
