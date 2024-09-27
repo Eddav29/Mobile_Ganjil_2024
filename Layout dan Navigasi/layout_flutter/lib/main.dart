908bb9d133c388f050b6ecb3d6f132b8ae94feac
Eddo Dava <eddodava@gmail.com>
2024-09-27 08:52:20 +0700

    Layout dan Navigasi : Praktikum 1 - Create project dan langkah 2

---
+++ Layout dan Navigasi/layout_flutter/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter layout: Nama dan NIM Anda',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
